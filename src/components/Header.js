0083b34ec06b4ecd91ed5e01a6d17ce00b02ff28
Elif Nur TÜRK <elifnurturk17@gmail.com>
2021-03-03 12:07:33 +0300

    Kodluyoruz Bitirme Projem
    Kodluyoruz Bitirme Projem

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+function Header({totalIncome}) {
+  return (
+    <header>
+      <h1>Gelir Listesi</h1>
+      <div className="total-income">£{totalIncome}</div>
+    </header>
+  );
+}
+
+export default Header;
+//rfce komutu ile kısayoldan fonk. oluşturulur.
