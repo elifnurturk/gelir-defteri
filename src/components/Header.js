03ffd7a039fa1f58abc4823724b46bb85684113f
Elif Nur TÜRK <69623465+elifistanya@users.noreply.github.com>
2021-03-24 17:00:26 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header({totalIncome}) {
   return (
     <header>
       <h1>Gelir Listesi</h1>
-      <div className="total-income">£{totalIncome}</div>
+      <div className="total-income">{totalIncome}</div>
     </header>
   );
 }
